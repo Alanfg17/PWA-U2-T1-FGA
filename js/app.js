d8617d49f3af23040395c92eeb428338cd6e12ce
Alanfg17 <78186472+Alanfg17@users.noreply.github.com>
2021-10-16 00:18:00 -0500

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,25 @@
+let principal = $('principal')
+let notice = $('notice')
+
+
+$('.btn-seguir').on('click', function(e){
+    e.preventDefault();
+    console.log('btn-seguir');
+    notice.fadeIn('slow',function() {
+        principal.slideUp(1000);
+    });
+});
+
+$('.btn-regresar').on('click', function() {
+   
+    notice.fadeOut(function(){
+
+        principal.slideDown(1000);
+    });
+
+    
+});
+
+if (navigator.serviceWorker) {
+    navigator.serviceWorker.register('/sw.js')
+}
---
+++ sw.js
@@ -0,0 +1,77 @@
+const CACHE_NAME = 'cache-v1'
+
+console.log('SW: Limpio');
+const CACHE_STATIC_NAME = 'static-v1'
+const DYMAMIC_CACHE_NAME = 'dynamic-v1'
+const CACHE_INMUTABLE_NAME = 'inmutable-v1'
+
+function cleanCache(cacheName,sizeItems) {
+    caches.open(cacheName)
+        .then(cache =>{
+            cache.keys().then(keys =>{
+                console.log(keys)
+                if (keys.length >= sizeItems) {
+                    cache.delete(keys[0].then(()=>{
+                        cleanCache(cacheName,sizeItems)
+                    }))
+                }
+            })
+            
+        })
+}
+
+self.addEventListener('install',(event) =>{
+    //Crear el caché y almacenar el APPSHELL
+    const promesaCache = caches.open(CACHE_NAME)
+        .then(cache => {
+            return cache.addAll([
+                '/',
+                'index.html',
+                'images/noticia1.png',
+                'images/noticia2.png',
+                'images/noticia3.png',
+                'images/noticia4.png',
+                'js/app.js'
+            ])
+        })
+
+    const promInmutable = caches.open(CACHE_INMUTABLE_NAME)
+        .then(cache => {
+            return cache.addAll([
+                'https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css',
+                'https://code.jquery.com/jquery-3.5.1.min.js',
+                'https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js'
+            ])
+        })
+    event.waitUntil(Promise.all([promesaCache,promInmutable]))
+})
+
+self.addEventListener('fetch',(event) =>{
+
+    //2. Caché cith network fallback
+    //Busca en caché y si no lo encuentra va a la red
+    const respuestaCache = caches.match(event.request)
+        .then(resp =>{
+            if (resp) {
+                return resp
+            }
+            console.log("No esta en caché ",event.request.url);
+
+            return fetch(event.request)
+                .then(respNet => {
+                    caches.open(DYMAMIC_CACHE_NAME)
+                        .then(cache =>{
+                            cache.put(event.request,respNet).then(ok =>{
+                                cleanCache(DYMAMIC_CACHE_NAME,10)
+                            })
+                            
+                        })
+                    return respNet.clone()
+                })
+        })
+    event.respondWith(respuestaCache)
+
+    //1. Only Caché
+    //event.respondWith(caches.match(event.request))
+})
+
